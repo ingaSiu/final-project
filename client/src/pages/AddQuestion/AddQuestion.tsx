ee9c0cc3247e40e321d7d709667abfc0587ee88a
ingaSiu <ingasiud@gmail.com>
2023-04-03 22:19:06 +0300

    pages added

---
+++ client/src/pages/AddQuestion/AddQuestion.tsx
@@ -0,0 +1,11 @@
+import AddQuestionForm from './AddQuestionForm';
+
+const AddQuestion = () => {
+  return (
+    <div>
+      <AddQuestionForm />
+    </div>
+  );
+};
+
+export default AddQuestion;
