8008be02df19682844002b1bfd574c6a2bc46655
ingaSiu <ingasiud@gmail.com>
2023-04-04 21:47:13 +0300

    add question component

--- client/src/pages/AddQuestion/AddQuestion.tsx
+++ client/src/pages/AddQuestion/AddQuestion.tsx
@@ -1,11 +1,45 @@
 import AddQuestionForm from './AddQuestionForm';
+import cat_question from '../../assets/cat_question.jpg';
+import styled from 'styled-components';
 
 const AddQuestion = () => {
   return (
-    <div>
-      <AddQuestionForm />
-    </div>
+    <Container>
+      <Wrapper>
+        <Details>
+          <Title>Got a question?</Title>
+          <AddQuestionForm />
+        </Details>
+
+        <img src={cat_question} alt="Cat with question" />
+      </Wrapper>
+    </Container>
   );
 };
 
 export default AddQuestion;
+
+const Wrapper = styled.div`
+  display: flex;
+  /* flex-wrap: wrap; */
+`;
+
+const Title = styled.p`
+  font-size: 40px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+  width: 100vw
+  height: 90vh;
+  align-items: center;
+`;
+
+const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
