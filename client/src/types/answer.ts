ee4d0c2bb41d2ba3190faf9d787cbe9dfe4c1805
ingaSiu <ingasiud@gmail.com>
2023-04-04 21:40:17 +0300

    types added

--- client/src/types/answer.ts
+++ client/src/types/answer.ts
@@ -1,9 +1,13 @@
 export type Answer = {
   _id?: string;
   answer: string;
-  rating: number;
-  createdAt: number;
-  updatedAt: number | null;
-  userId: string;
-  questionId: string;
+  rating?: number;
+  createdAt?: number;
+  updatedAt?: number | null;
+  userId?: string;
+  questionId?: string;
+};
+
+export type NewAnswer = {
+  answer: string;
 };
